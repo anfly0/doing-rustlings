1101a113c0b382b09cd21064ef824e51ac356553
viktor <viktor@localhost.localdomain>
2024-08-03 22:14:35 +0200

    init

---
+++ exercises/02_functions/functions1.rs
@@ -0,0 +1,8 @@
+// TODO: Add some function with the name `call_me` without arguments or a return value.
+fn call_me() {
+    println!("I got the call..")
+}
+
+fn main() {
+    call_me(); // Don't change this line
+}
